45d1c3a6aa117daa305d828a8e11a7cf840ed32c
Erwan Lemonnier <erwan@lemonnier.se>
2017-09-07 19:45:05 +0200

    Now with a crash endpoint

--- helloworld/api.py
+++ helloworld/api.py
@@ -7,3 +7,7 @@ log = logging.getLogger(__name__)
 
 def do_hello():
     return ApiPool.helloworld.model.Hello(message='Hello world!')
+
+
+def do_crash():
+    raise Exception("OH! NO! I JUST DIED!!")
