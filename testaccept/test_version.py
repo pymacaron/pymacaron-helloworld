47af6b1dc98fa9ca799e404d8529f7515015581b
Erwan Lemonnier <erwan@lemonnier.se>
2017-09-07 23:46:01 +0200

    First acceptance tests

--- testaccept/test_version.py
+++ testaccept/test_version.py
@@ -1,18 +1,24 @@
-from pnt_common.test import PntTest
+import logging
+import os
+from klue_microservice.test import KlueMicroServiceTestCase
 
-class Test(PntTest):
+log = logging.getLogger(__name__)
+
+class Test(KlueMicroServiceTestCase):
 
     def setUp(self):
         super(Test, self).setUp()
+        self.verify_ssl = True if 'klue.' in os.environ.get('PNT_SERVER_HOST', '') else False
+        log.debug("TEST: verify_ssl=%s" % self.verify_ssl)
 
     def test_ping(self):
         self.assertHasPing()
 
     def test_version(self):
-        self.assertHasVersion()
+        self.assertHasVersion(verify_ssl=self.verify_ssl)
 
-    def test_auth_version(self):
-        self.assertHasAuthVersion()
+     def test_auth_version(self):
+         self.assertHasAuthVersion(verify_ssl=self.verify_ssl)
 
 if __name__ == '__main__':
     unittest.main()
