ea0612d36b09355359c1dff23dd69d65fad57739
Erwan Lemonnier <erwan@lemonnier.se>
2017-09-07 22:41:51 +0200

    Set jet secret when defining api

--- server.py
+++ server.py
@@ -18,7 +18,7 @@ def main(port, debug):
     path_apis = os.path.join(here, "apis")
 
     app = Flask(__name__)
-    api = API(app, port=port, debug=debug)
+    api = API(app, port=port, debug=debug, jwt_secret='12345123451234512345')
     api.load_apis(path_apis)
     api.start(serve="helloworld")
 
