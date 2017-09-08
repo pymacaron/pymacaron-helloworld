a14828eeded8205b306ae43aeab978ad4c0c8265
Erwan Lemonnier <erwan@lemonnier.se>
2017-09-08 16:41:11 +0200

    app must be global

--- server.py
+++ server.py
@@ -7,13 +7,13 @@ from klue_microservice import API, letsgo
 
 log = logging.getLogger(__name__)
 
+app = Flask(__name__)
 
 def start(port, debug):
 
     here = os.path.dirname(os.path.realpath(__file__))
     path_apis = os.path.join(here, "apis")
 
-    app = Flask(__name__)
     api = API(
         app,
         port=port,
